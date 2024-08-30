1bd3658ccdb71c18e74399ab609035dc908c3e15
Javier Gutiérrez <ggutierrez.javier@gmail.com>
2024-08-30 10:35:20 -0400

    Add project files.

---
+++ Controllers/JSONParserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JSONParser.Controllers
+{
+    public class JSONParserController : Controller
+    {
+        // POST: JSONParserController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ParseJSON([FromBody] string json)
+        {
+            //var tokens
+            return Json(json);
+        }
+    }
+}
